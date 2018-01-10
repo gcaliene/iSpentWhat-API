bb82ba9d685d9287b001e4ea5bf059691ff95343
Gerson Calienes <gersoncalienes@gmail.com>
2018-01-09 19:57:03 -0500

    deleted some comments

--- config.js
+++ config.js
@@ -7,12 +7,3 @@ exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-
-// module.exports = {
-//   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-// };
-// //
-// module.exports = { PORT: process.env.PORT || 8080 };
-//
-// module.exports = { JWT_SECRET: process.env.JWT_SECRET || 'herdfgdfgdf' };
-// module.exports = { JWT_EXPIRY: process.env.JWT_EXPIRY || '7d' };
